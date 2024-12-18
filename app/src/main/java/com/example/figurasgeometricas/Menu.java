75b4f5ae5aa376b57c2a5f5dd50074acac54f143
Milton <milton.ramos954@ist17dejulio.edu.ec>
2024-12-17 21:32:56 -0500

    Funcional el menu de figuras y creacion de actividad para el calculo del area del Trapecio

--- app/src/main/java/com/example/figurasgeometricas/Menu.java
+++ app/src/main/java/com/example/figurasgeometricas/Menu.java
@@ -1,6 +1,8 @@
 package com.example.figurasgeometricas;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +11,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class Menu extends AppCompatActivity {
+    Button btn1, btn2, btn3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +22,24 @@ public class Menu extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        btn1 = findViewById(R.id.btnCuadrado);
+        btn2 = findViewById(R.id.btnTriangulo);
+        btn3 = findViewById(R.id.btnCirculo);
+
+        btn1.setOnClickListener(v -> {
+            Intent intent = new Intent(Menu.this, MainActivity.class);
+            startActivity(intent);
+        });
+
+        btn2.setOnClickListener(v -> {
+            Intent intent = new Intent(Menu.this, Triangulo.class);
+            startActivity(intent);
+        });
+
+        btn3.setOnClickListener(v -> {
+            Intent intent = new Intent(Menu.this, Circulo.class);
+            startActivity(intent);
         });
     }
 }
