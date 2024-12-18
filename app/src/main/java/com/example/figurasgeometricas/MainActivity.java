06e00c912021b5da91c20c542c18eb08ac5a8b8f
Milton <milton.ramos954@ist17dejulio.edu.ec>
2024-12-17 19:06:11 -0500

    Creacion para calcular el area del cuadrado con el servicio servido a cliente

---
+++ app/src/main/java/com/example/figurasgeometricas/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.figurasgeometricas;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView txtl ,Texto;
+    Button btnCua;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        txtl = findViewById(R.id.txtLado);
+        btnCua = findViewById(R.id.btnCua);
+        Texto = findViewById(R.id.txtResul);
+
+        btnCua.setOnClickListener(v -> {
+            String numero = txtl.getText().toString().trim();
+            if (!numero.isEmpty()) {
+                obtenerServicioWeb("http://192.168.68.104:3001/cuadrado/" + numero);
+            } else {
+                Toast.makeText(getApplicationContext(), "Por favor ingresa un número", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+    
+
+}
